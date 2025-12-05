80590e547190def7b3dc41f3463019b9cb7dae43
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-05 11:47:36 -0600

    Update system.mjs

--- system.mjs
+++ system.mjs
@@ -64,6 +64,7 @@ Hooks.once("init", async () => {
     "systems/marks-of-mezoria/templates/actor/parts/drops/backdrop.hbs",
     "systems/marks-of-mezoria/templates/actor/parts/drops/markpurpose.hbs",
     "systems/marks-of-mezoria/templates/actor/parts/drops/tribedrop.hbs",
+    "systems/marks-of-mezoria/templates/actor/parts/drops/clandrop.hbs",
 
     // Character Info tab + subparts
     "systems/marks-of-mezoria/templates/actor/parts/cinfo.hbs",
