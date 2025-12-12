2621ea3f0b9adfb1d8bf4e35bf88464909071f7c
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-12 16:47:38 -0600

    Create marks-of-power.mjs

---
+++ scripts/marks-of-power.mjs
@@ -0,0 +1,118 @@
+// scripts/marks/marks-of-power.mjs
+// Marks of Power – gained at Garnet Rank
+// These are tangible, externalized expressions of power.
+
+export const MarksOfPower = {
+
+  weapons: [
+    "Axe",
+    "Bow",
+    "Claw",
+    "Fork",
+    "Gun",
+    "Hammer",
+    "Hook",
+    "Knife",
+    "Net",
+    "Rake",
+    "Sceptre",
+    "Shield",
+    "Shovel",
+    "Sickle",
+    "Spear",
+    "Spike",
+    "Staff",
+    "Sword",
+    "Thread",
+    "Trap",
+    "Trowel",
+    "Whip"
+  ],
+
+  armor_materials_objects: [
+    "Armour",
+    "Cage",
+    "Chain",
+    "Cloth",
+    "Iron",
+    "Paper",
+    "Ship",
+    "Technology",
+    "Vehicle",
+    "Wall",
+    "Wheel",
+    "Wood"
+  ],
+
+  elements: [
+    "Cold",
+    "Earth",
+    "Fire",
+    "Ice",
+    "Light",
+    "Lightning",
+    "Water",
+    "Wind"
+  ],
+
+  animals_creatures: [
+    "Ape",
+    "Bat",
+    "Bear",
+    "Bee",
+    "Bird",
+    "Cat",
+    "Cattle",
+    "Crocodile",
+    "Deer",
+    "Dog",
+    "Duck",
+    "Fish",
+    "Flea",
+    "Fox",
+    "Frog",
+    "Goat",
+    "Horse",
+    "Lizard",
+    "Locust",
+    "Lurker",
+    "Manatee",
+    "Monkey",
+    "Mouse",
+    "Octopus",
+    "Pangolin",
+    "Rabbit",
+    "Rat",
+    "Shark",
+    "Skunk",
+    "Sloth",
+    "Snake",
+    "Spider",
+    "Turtle",
+    "Wasp",
+    "Whale",
+    "Wolf"
+  ],
+
+  plants_terrain: [
+    "Coral",
+    "Deep",
+    "Fungus",
+    "Growth",
+    "Plant",
+    "Renewal",
+    "Tree"
+  ],
+
+  body_physicality: [
+    "Foot",
+    "Hand",
+    "Life",
+    "Might"
+  ],
+
+  magic_metaphysics: [
+    "Magic",
+    "Pure"
+  ]
+};
